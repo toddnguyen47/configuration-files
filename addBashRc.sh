48f9deea25cd05a68057f6bf54b5f951dfcb77fe
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2020-01-16 23:07:19 -0800

    Create addBashRc.sh

---
+++ addBashRc.sh
@@ -0,0 +1,3 @@
+export PS1="\[$(tput bold)\]\[\033[38;5;11m\]\u\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\]@\h:  \[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;14m\]\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] [\A]\n\\$ \[$(tput sgr0)\]"
+
+alias rm='trash'
