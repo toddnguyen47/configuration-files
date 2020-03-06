8495e8a56f627f90e3e181c74b19d47170632454
Todd Nguyen <todd.nguyen47@gmail.com>
2020-03-06 08:54:58 -0800

    Informative fish prompt

--- fish-shell/fish_prompt.fish
+++ fish-shell/fish_prompt.fish
@@ -1,8 +1,55 @@
-# Ref: https://github.com/fish-shell/fish-shell/issues/3480
-function shorten_hostname --description "Shorten the hostname"
-        if not set -q __fish_prompt_hostname
-                set -g __fish_prompt_hostname (hostname | cut -c 1-5)(echo ...)
-        end
-        #set -l color_dir cyan
-        set -g fish_prompt_pwd_dir_length 0
+function fish_prompt --description 'Write out the prompt'
+	#Save the return status of the previous command
+    set stat $status
+
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    if not set -q __fish_color_blue
+        set -g __fish_color_blue (set_color -o blue)
+    end
+
+    #Set the color for the status depending on the value
+    set __fish_color_status (set_color -o green)
+    if test $stat -gt 0
+        set __fish_color_status (set_color -o red)
+    end
+
+    switch "$USER"
+
+        case root toor
+
+            if not set -q __fish_prompt_cwd
+                if set -q fish_color_cwd_root
+                    set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+                else
+                    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+                end
+            end
+
+            printf '%s@%s %s%s%s# ' \
+              $USER (prompt_hostname) \
+              "$__fish_prompt_cwd" \
+              (prompt_pwd) \
+              "$__fish_prompt_normal"
+
+        case '*'
+
+            if not set -q __fish_prompt_cwd
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+            end
+
+            printf '<< [%s] %s%s@%s >>\n%s%s %s(%s)%s \f\r↳ ' \
+              (date "+%H:%M:%S") \
+              "$__fish_color_blue" \
+              $USER (prompt_hostname) \
+              "$__fish_prompt_cwd" \
+              "$PWD" \
+              "$__fish_color_status" \
+              "$stat" \
+              "$__fish_prompt_normal"
+
+    end
 end
+
