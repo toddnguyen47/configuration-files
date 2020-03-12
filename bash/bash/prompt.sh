77f9d9c10106064bc4a7a1cb7d12e19bf7c84389
Todd Nguyen <todd.nguyen47@gmail.com>
2020-03-12 08:09:47 -0700

    Added associated arrays of fg colors

--- bash/bash/prompt.sh
+++ bash/bash/prompt.sh
@@ -1,45 +1,60 @@
 #!/bin/bash
 
+# Ref: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+# Use an associated array (hash table) for colors
+declare -A fg_colors
+fg_colors[default]='\[\e[0m\]'
+fg_colors[black]='\[\e[0;30m\]'
+fg_colors[red]='\[\e[0;31m\]'
+fg_colors[green]='\[\e[0;32m\]'
+fg_colors[yellow]='\[\e[0;33m\]'
+fg_colors[blue]='\[\e[0;34m\]'
+fg_colors[magenta]='\[\e[0;35m\]'
+fg_colors[cyan]='\[\e[0;36m\]'
+fg_colors[white]='\[\e[0;37m\]'
+
+declare -A fg_colors_bold
+fg_colors_bold[default]='\[\e[0m\]'
+fg_colors_bold[black]='\[\e[1;30m\]'
+fg_colors_bold[red]='\[\e[1;31m\]'
+fg_colors_bold[green]='\[\e[1;32m\]'
+fg_colors_bold[yellow]='\[\e[1;33m\]'
+fg_colors_bold[blue]='\[\e[1;34m\]'
+fg_colors_bold[magenta]='\[\e[1;35m\]'
+fg_colors_bold[cyan]='\[\e[1;36m\]'
+fg_colors_bold[white]='\[\e[1;37m\]'
+
 # Ref: https://stackoverflow.com/a/16715681
-PROMPT_COMMAND=__prompt_command # Func to gen PS1 after CMDs
+PROMPT_COMMAND=__prompt_command # Func to generate PS1 after CMDs
 
 __prompt_command() {
   local EXIT="$?" # This needs to be first
   PS1=""
 
   local promptChar='$'
-
-  local ResetColor='\[\e[0m\]'
-
-  local Red='\[\e[0;31m\]'
-  local Green='\[\e[0;32m\]'
-  local BoldGreen='\[\e[1;32m\]'
-  local BoldYellow='\[\e[1;33m\]'
-  local BoldBlue='\[\e[1;34m\]'
-  local Purple='\[\e[0;35m\]'
-  local Space=' '
-  local NewLine='\n'
+  local space=' '
+  local newline='\n'
 
   # Add user@hostname
-  PS1+="${BoldGreen}\u@\h${Space}"
+  PS1+="${fg_colors_bold[green]}\u@\h${space}"
 
   # Add directory
-  PS1+="${BoldBlue}\w${Space}"
+  PS1+="${fg_colors_bold[blue]}\w${space}"
 
   # Add time
-  PS1+="${ResetColor}⌚ \A"
+  PS1+="${fg_colors[default]}⌚ \A"
 
-  PS1+="${NewLine}"
+  PS1+="${newline}"
 
   # Add prompt now!
   if [ $EXIT != 0 ]; then
-    PS1+="${Red}" # Add red if exit code non 0
+    PS1+="${fg_colors[red]}" # Add red if exit code non 0
     promptChar='✘'
   fi
   PS1+="${promptChar}"
 
 
   # Reset color for user
-  PS1+="${ResetColor}${Space}"
+  PS1+="${fg_colors[default]}${space}"
 }
 
