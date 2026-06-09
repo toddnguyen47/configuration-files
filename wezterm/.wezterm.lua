057116b9b4c3c8526e4c863dddf02e2e97c527e3
Todd Nguyen-Tran <todd.nguyen47@gmail.com>
2026-06-09 15:09:41 -0700

    Added wezterm configuration

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 15
+config.font = wezterm.font("MonaspiceAr Nerd Font", { weight = "Medium" })
+config.line_height = 1.2
+-- config.color_scheme = "Desert (Gogh)"
+config.color_scheme = "Dracula (Official)"
+
+-- Finally, return the configuration to wezterm:
+return config
