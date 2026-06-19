b68ad419a5624e60b5c09e6fbacecbd1cd2b7657
Todd Nguyen-Tran <todd.nguyen47@gmail.com>
2026-06-18 17:08:21 -0700

    Update .wezterm.lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,8 +7,8 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+config.initial_cols = 80
+config.initial_rows = 24
 
 -- or, changing the font size and color scheme.
 config.font_size = 15
@@ -16,6 +16,29 @@ config.font = wezterm.font("MonaspiceAr Nerd Font", { weight = "Medium" })
 config.line_height = 1.2
 -- config.color_scheme = "Desert (Gogh)"
 config.color_scheme = "Dracula (Official)"
+
+-- Only on MacOS / Linux
+config.use_resize_increments = true
+
+-- 2. Custom Startup Layout
+wezterm.on("gui-startup", function(cmd)
+	-- Create the window and the "Primary" Tab (Single pane)
+	local tab1, pane1, window = wezterm.mux.spawn_window(cmd or {})
+	tab1:set_title("Primary")
+
+	-- Create the "Secondary" Tab
+	local tab2, pane2, _ = window:spawn_tab(cmd or {})
+	tab2:set_title("Secondary")
+
+	-- Split the "Secondary" Tab vertically (creates Left and Right panes)
+	pane2:split({
+		direction = "Right",
+		size = 0.5,
+	})
+
+	-- Bring focus back to the Primary Tab on startup
+	tab1:activate()
+end)
 
 -- Finally, return the configuration to wezterm:
 return config
