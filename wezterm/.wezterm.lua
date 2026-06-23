73f2b5d630313dbba1e516918cccd3699be29eb8
Todd Nguyen-Tran <todd.nguyen47@gmail.com>
2026-06-23 08:33:03 -0700

    Set wezterm's the initial rows and columsn to 100 and 28

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,8 +7,8 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 80
-config.initial_rows = 24
+config.initial_cols = 100
+config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 15
