36ac86626cefe453a79156ab4639d41bec2cacc0
Todd Nguyen-Tran <todd.nguyen47@gmail.com>
2026-07-22 16:16:45 -0700

    Fix lazyvim mappings

--- vim/lazyvim/lua/config/keymaps.lua
+++ vim/lazyvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("v", "y", [["+y]], { desc = "Yank visual selection to system clipboard" })
-vim.keymap.set("v", "p", [["+p]], { desc = "Paste system clipboard over visual selection" })
-vim.keymap.set("n", "p", [["+p]], { desc = "Paste system clipboard over visual selection" })
+vim.keymap.set("v", "<leader>y", [["+y]], { desc = "Yank visual selection to system clipboard" })
+vim.keymap.set("v", "<leader>p", [["+p]], { desc = "Paste system clipboard over visual selection" })
+vim.keymap.set("n", "<leader>p", [["+p]], { desc = "Paste system clipboard over visual selection" })
